ec4b87ca394f5fe7a25b1058d22b07d9435de277
Pedro Vieira <pedronicolas576@gmail.com>
2025-03-27 16:54:29 -0300

    Adicionar arquivos de projeto.

---
+++ Teste/Models/UsuarioFuncionario.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Teste.Models;
+
+namespace Funcionario
+{
+    class UsuarioFuncionario : Pessoa
+    {
+        int id = 0;
+
+        public UsuarioFuncionario(string nome, int idade) : base(nome, idade)
+        {
+            
+        }
+
+        public void Longevidade()
+        {
+            if (Idade >= 18)
+            {
+                Console.WriteLine("Velho");
+                id = 1;
+            }
+            else
+            {
+                Console.WriteLine("Novo");
+            }
+        }
+    }
+}
---
+++ Teste/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Funcionario;
+using Teste.Models;
+
+Console.WriteLine("Hello, World!");
+
+Pessoa p1 = new Pessoa(nome: "Pedro", idade: 22);
+
+p1.Apresentar();
+
+UsuarioFuncionario p2 = new UsuarioFuncionario(nome: "Pedro", idade: 22);
+
+p2.Longevidade();
